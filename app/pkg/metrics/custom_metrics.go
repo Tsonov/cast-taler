9a2af689a3f04b68da4adff3a8ced15653b8c791
Roman Melnyk <roman.melnyk20071992@gmail.com>
2025-07-22 22:47:55 +0300

    fix metrics

--- app/pkg/metrics/custom_metrics.go
+++ app/pkg/metrics/custom_metrics.go
@@ -11,15 +11,15 @@ var trafficCounter = prometheus.NewCounterVec(
 		Name: "traffic_total",
 		Help: "Total bytes sent and received by protocol, source pod, source az, target az, target pod, and success.",
 	},
-	[]string{"success", "protocol", "source-pod", "source-az", "target-az", "target-pod"})
+	[]string{"success", "protocol", "source_pod", "source_az", "target_az", "target_pod"})
 
 func TrackTraffic(bytes float64, success bool, protocol string, sourcePod string, sourceAz string, targetAz string, targetName string) {
 	trafficCounter.With(prometheus.Labels{
 		"success":    strconv.FormatBool(success),
 		"protocol":   protocol,
-		"source-pod": sourcePod,
-		"source-az":  sourceAz,
-		"target-az":  targetAz,
-		"target-pod": targetName,
+		"source_pod": sourcePod,
+		"source_az":  sourceAz,
+		"target_az":  targetAz,
+		"target_pod": targetName,
 	}).Add(bytes)
 }
