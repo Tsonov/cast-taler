364156e1a93fbfa22c81616adcc805ff74f33227
Lachezar Tsonov <lachezar@cast.ai>
2025-07-23 11:35:06 +0300

    optimizer skeleton

--- optimizer/main.go
+++ optimizer/main.go
@@ -1,7 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/pflag"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Define command-line flags
+	var kubeconfig string
+	var kubecontext string
+
+	// Set default kubeconfig path if not specified
+	if home := homedir.HomeDir(); home != "" {
+		pflag.StringVar(&kubeconfig, "kubeconfig", filepath.Join(home, ".kube", "config"),
+			"(optional) absolute path to the kubeconfig file")
+	} else {
+		pflag.StringVar(&kubeconfig, "kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	pflag.StringVar(&kubecontext, "kubecontext", "", "name of the kubeconfig context to use")
+	pflag.Parse()
+
+	// Create the Kubernetes client configuration
+	var config *rest.Config
+	var err error
+
+	if kubeconfig != "" {
+		// Use out-of-cluster configuration with the provided kubeconfig
+		loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
+		loadingRules.ExplicitPath = kubeconfig
+
+		configOverrides := &clientcmd.ConfigOverrides{}
+		if kubecontext != "" {
+			configOverrides.CurrentContext = kubecontext
+		}
+
+		clientConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, configOverrides)
+		config, err = clientConfig.ClientConfig()
+		if err != nil {
+			fmt.Printf("Error creating Kubernetes client config from kubeconfig: %v\n", err)
+			os.Exit(1)
+		}
+	} else {
+		// Use in-cluster configuration
+		config, err = rest.InClusterConfig()
+		if err != nil {
+			fmt.Printf("Error creating in-cluster config: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	// Create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Printf("Error creating Kubernetes client: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Test the connection by listing nodes
+	nodes, err := clientset.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		fmt.Printf("Error listing nodes: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Successfully connected to Kubernetes API. Found %d nodes.\n", len(nodes.Items))
+
+	// Your optimizer logic goes here
+	fmt.Println("Optimizer started")
 }
